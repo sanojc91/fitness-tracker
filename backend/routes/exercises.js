e486d3081d29d8a4f9eb03c8c25b903f6335f681
Sanoj Chathuranga Amarasiri <chathuranga.amarasiri91@gmail.com>
2020-12-06 22:26:47 +0530

    DB working checked with users collection

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,9 +1,11 @@
-const router = require('express').Router;
+const router = require('express').Router();
 let Exercise = require('../models/exercise.model');
 
 router.route('/').get((req, res) => {
-    Exercise.find().then(exercises => res.json(exercises)).catch(err => res.status(400).json('Error: '+ err));
-});
+    Exercise.find()
+      .then(exercises => res.json(exercises))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
 
 router.route('/add').post((req, res) => {
 
--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
-app.use('./users', usersRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on Port : ${port}`);
