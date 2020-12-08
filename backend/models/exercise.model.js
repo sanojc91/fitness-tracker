dac6e97943ee511e74d265df63d0bcfe1ca85751
Sanoj Chathuranga Amarasiri <chathuranga.amarasiri91@gmail.com>
2020-12-08 21:36:41 +0530

    backend completed

--- backend/models/exercise.model.js
+++ backend/models/exercise.model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
     username: { type: String, required: true },
-    discription: { type: String, required: true },
+    description: { type: String, required: true },
     duration: { type: Number, required:true },
     date: { type: Date, required: true },
 }, {
