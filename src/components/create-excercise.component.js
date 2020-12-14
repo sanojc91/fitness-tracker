477dbbec9135243a784a2b1caa2e9b9015947b61
Sanoj Chathuranga Amarasiri <chathuranga.amarasiri91@gmail.com>
2020-12-14 22:57:34 +0530

    components created

---
+++ src/components/create-excercise.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+export default class CreateExercise extends Component {
+    render(){
+        return(
+            <div>
+                <p>Create Exercise Component</p>
+            </div>
+        );
+    }
+}
